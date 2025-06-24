9ecf957f49453b387a3a8cc1e6fcc70446e4cbc8
Shrimant Sutar <136959327+shrimantsutar@users.noreply.github.com>
2025-06-24 19:15:30 +0000

    datatypes summary done

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,58 @@
+// Primative DataType
+
+// 7 Types : String, Number, Boolean, null, undefines, Symbol, BigInt
+
+// String
+const name="Shrimant"
+
+// Number
+const score=100
+const scoreavalue=300.2
+
+// Boolean
+const inLogin =false
+
+// Null
+const outsideTemp= null
+
+// Undefined
+let usedEmail;
+
+// Symbol
+const id = Symbol(1234);
+const anotherId = Symbol(1234);
+
+console.log(id === anotherId);
+
+// BigInt
+const bigNumber =12837451827451872n
+
+
+
+// Referance DataType
+
+// Array, Objests, Funstions
+
+
+// Array
+const heros=["shktiman","aaryaman","salman","shah rukh","Aamir khan"]
+
+// Object 
+
+let mydetails={
+    Name: "shree",
+    age: 23,
+    address: " N-2,Mahalaxmi Chowk", 
+}
+
+//Funstions
+
+const Myfunstion= function(){
+    console.log("Hello World");
+    
+}
+
+// how to find data type of any dataypes
+
+console.log(typeof mydetails )
+
