7a22fceca96a440496b2e6256c222b31686a6ea7
Shrimant Sutar <136959327+shrimantsutar@users.noreply.github.com>
2025-06-24 18:38:37 +0000

    Conversion_Operations Done

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,86 @@
+let score ="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// console.log(Number(score));
+
+let ValueInNumber =Number(score);
+// console.log(typeof ValueInNumber);
+
+// console.log(ValueInNumber);
+
+// "33"   =>  33
+// "33abc"   =>   NaN
+//  ture   =>   1
+//  false   => 0
+
+
+
+let isLoogIn = ""
+
+// console.log (booleanConvert= Boolean (isLoogIn));
+// console.log(booleanConvert);
+
+// 1  => true;
+// 0  => false;
+
+// ""  => false
+// "shri"   => true
+
+
+
+let someNumber = 12;
+let     stringNumber =String (someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************** Operations*******************
+
+
+let value= 13;
+let nagativeValue = -value
+
+// console.log(nagativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+
+let str1="Shri"
+let str2=" Sutar"
+
+let str3= str1+str2;
+// console.log(str3);
+
+
+// console.log(2+"1");
+// console.log("2"+1);
+// console.log(2+"1"+2);
+// console.log("2"+1+2);
+// console.log(1+2+"1");
+
+
+// console.log(+ture);
+// console.log(+"");
+
+
+let gamecounter=20;
+
+gamecounter++;
+
+console.log(gamecounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
